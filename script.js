0b3a0381db92b8e491a85c653daed7cc40f20259
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-01 22:04:14 +0200

    [add] addTask and isError

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
             tasks: [
-                // Array di oggetti che rappresentano i todo
-                { text: 'Pagare la bolletta' },
-                { text: 'Studiare Vue' },
-                { text: 'Comprare la pappa di Artur' }
+                { text: 'Fare la spesa', isError: false },
+                { text: 'Studiare Vue',  isError: false },
+                { text: 'Comprare la pappa di Artu', isError: false }
             ],
-        }
+            newTask: '',
+            isError: false
+        };
     },
+
     methods: {
-       
+        addTask() {
+            if (this.newTask.length < 5) {
+                // Bonus 1: Messaggio di errore se il task ha meno di 5 caratteri
+                this.isError = true;
+            } else {
+                // Aggiungi un nuovo task alla lista
+                this.tasks.unshift({ text: this.newTask });
+                // Resetta il campo di input
+                this.newTask = '';
+                // Resetta l'errore se era stato visualizzato
+                this.isError = false;
+            }
+        },
+
+        
+        
     }
 }).mount('#app');

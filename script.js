ab9c21b0091b090532b95feb6acbc8f02c0d77e7
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-01 21:34:19 +0200

    [add] arrey oggetti

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            
+            tasks: [
+                // Array di oggetti che rappresentano i todo
+                { text: 'Pagare la bolletta' },
+                { text: 'Studiare Vue' },
+                { text: 'Comprare la pappa di Artur' }
+            ],
         }
     },
     methods: {
